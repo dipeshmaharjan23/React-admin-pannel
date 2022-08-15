5b47d312852ade108af0a22ff085998b28934a58
dipesh maharjan <max.maharjan.2014@gmail.com>
2022-08-15 16:16:35 +0545

    new page added

---
+++ src/formSource.js
@@ -0,0 +1,76 @@
+export const userInputs =[
+    {
+        id: 1,
+        label :'Username',
+        type : 'text',
+        placeholder :'john_doe'
+    }, 
+    {
+        id: 2,
+        label :'Name',
+        type : 'text',
+        placeholder :'John Doe'
+    },
+    {
+        id: 3,
+        label :'Email',
+        type : 'text',
+        placeholder :'john_doe@gmail.com'
+    },
+    {
+        id: 4,
+        label :'Phone',
+        type : 'number',
+        placeholder :'+9887555'
+    },
+    {
+        id: 5,
+        label :'Password',
+        type : 'password',
+    },
+    {
+        id: 6,
+        label :'Address',
+        type : 'text',
+        placeholder :'kathmandu'
+    },
+    {
+        id: 7,
+        label :'Country',
+        type : 'text',
+        placeholder :'nepal'
+    },
+]
+
+export const productInputs =[
+    {
+        id :1,
+        label : "Title",
+        type :"text",
+        placeholder:'Dell'
+    },
+    {
+        id :2,
+        label : "Description",
+        type :"text",
+        placeholder:'Description'
+    },
+    {
+        id :3,
+        label : "Category",
+        type :"text",
+        placeholder:'laptop'
+    },
+    {
+        id :4,
+        label : "Price",
+        type :"text",
+        placeholder:'100'
+    },
+    {
+        id :6,
+        label : "Stock",
+        type :"text",
+        placeholder:'in stock'
+    },
+  ]
--- src/pages/new/New.jsx
+++ src/pages/new/New.jsx
@@ -1,8 +1,48 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Navbar from '../../components/navbar/Navbar'
+import Sidebar from '../../components/sidebar/Sidebar'
+import './new.scss';
+import DriveFolderUploadOutlinedIcon from '@mui/icons-material/DriveFolderUploadOutlined';
 
-const New = () => {
+const New = ({ inputs, title }) => {
+
+  const [file ,setFile] = useState("");
+  console.log(file);
+
   return (
-    <div>New</div>
+    <div className='new'>
+      <Sidebar />
+      <div className="sideContainer">
+        <Navbar />
+        <div className="top">
+          <h1>{title}</h1>
+        </div>
+        <div className="bottom">
+          <div className="left">
+            <img src="https://icon-library.com/images/no-image-icon/no-image-icon-0.jpg" alt="no avatar" />
+          </div>
+          <div className="right">
+            <form >
+              <div className="formInput">
+                <label htmlFor='file' >Image :<DriveFolderUploadOutlinedIcon className='icon' /></label>
+                <input type="file" id='file' onChange={e=>setFile(e.target.files[0])} style={{ display: 'none' }} />
+              </div>
+
+              {inputs.map((input) => {
+                return (
+                    <div className="formInput" key={input.id}>
+                      <label >{input.label}</label>
+                      <input type={input.type} placeholder={input.placeholder} />
+                    </div>
+                )
+              })
+              }
+              <button>Send</button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
