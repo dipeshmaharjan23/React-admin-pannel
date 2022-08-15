05b706052e452d5cd5d8db1aa6a3d0cf7ad6178a
dipesh maharjan <max.maharjan.2014@gmail.com>
2022-08-15 19:34:14 +0545

    darkmode adjust

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,13 +1,16 @@
 import React from 'react'
 import './navbar.scss'
 import SavedSearchIcon from '@mui/icons-material/SavedSearch';
-import DarkModeIcon from '@mui/icons-material/DarkMode';
-import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
+import DarkModeOutlinedIcon from '@mui/icons-material/DarkModeOutlined';import NotificationsNoneIcon from '@mui/icons-material/NotificationsNone';
 import EmailOutlinedIcon from '@mui/icons-material/EmailOutlined';
 import FullscreenOutlinedIcon from '@mui/icons-material/FullscreenOutlined';
 import TocOutlinedIcon from '@mui/icons-material/TocOutlined';
+import { useContext } from 'react';
+import { DarkModeContext } from '../context/darkModeContext';
 
 const Navbar = () => {
+  const { dispatch } = useContext(DarkModeContext)
+
   return (
     <div className='navbar'>
       <div className="wrapper">
@@ -20,7 +23,7 @@ const Navbar = () => {
         <div className="items">
 
           <div className="item">
-            <DarkModeIcon className='icon' />
+            <DarkModeOutlinedIcon className='icon' onClick={() => dispatch({ type: "Toggle" })}/>
           </div>
           <div className="item">
             <NotificationsNoneIcon className='icon' />
